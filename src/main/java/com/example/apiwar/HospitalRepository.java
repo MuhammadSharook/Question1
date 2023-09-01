ce1a1efbb1cb03e5b3d09a13af0ec95385e2d9eb
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-09-01 19:28:09 +0530

    Complete

--- src/main/java/com/example/apiwar/HospitalRepository.java
+++ src/main/java/com/example/apiwar/HospitalRepository.java
@@ -2,6 +2,16 @@ package com.example.apiwar;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public class HospitalRepository {
+
+    Map<String,Doctor> doctorDb = new HashMap<>();
+    Map<String,Appointment> appointmentDb = new HashMap<>();
+    Map<String,Hospital> hospitalDb = new HashMap<>();
+    Map<String, List<String>> doctorAppointmentsDb = new HashMap<>();
+    Map<String, List<String>> centerDoctorsDb = new HashMap<>();
 }
